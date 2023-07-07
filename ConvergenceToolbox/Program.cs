2ebc2bf21de5dea6d312d140fce97faaa07e5127
SkyMound <crafteurkiller@gmail.com>
2023-07-07 16:20:50 +0200

    Added Main program (not tested)

--- ConvergenceToolbox/Program.cs
+++ ConvergenceToolbox/Program.cs
@@ -8,9 +8,61 @@ namespace ConvergenceToolbox
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Here will the program to inject the tools to the process Convergence.exe");
+            string targetProcessName = "Convergence.exe";
+            string assemblyPath = "Tools.dll";
+            string namespace = "Tools";
+            string className = "ToolsLoader";
+            string methodName = "Init";
+
+            Injector injector = new Injector(targetProcessName);
+
+            // Perform the injection
+            Inject(injector, assemblyPath, namespace, className, methodName);
+
+        }
+
+        private static void Inject(Injector injector, string assemblyPath, string namespace, string className, string methodName)
+        {
+            byte[] assembly;
+            using(injector){
+
+                try
+                {
+                    assembly = File.ReadAllBytes(assemblyPath);
+                }
+                catch
+                {
+                    System.Console.WriteLine("Could not read the file " + assemblyPath);
+                    return;
+                }
+
+                IntPtr remoteAssembly = IntPtr.Zero;
+
+                try
+                {
+                    remoteAssembly = injector.Inject(assembly, namespace, className, methodName);
+                }
+                catch (InjectorException ie)
+                {
+                    System.Console.WriteLine("Failed to inject assembly: " + ie);
+                    return;
+                }
+                catch (Exception exc)
+                {
+                    System.Console.WriteLine("Failed to inject assembly (unknown error): " + exc);
+                    return;
+                }
+
+                if (remoteAssembly == IntPtr.Zero)
+                    return;
+
+                System.Console.WriteLine($"{Path.GetFileName(assemblyPath)}: " +
+                    (injector.Is64Bit
+                    ? $"0x{remoteAssembly.ToInt64():X16}"
+                    : $"0x{remoteAssembly.ToInt32():X8}"));
+            }
         }
     }
 }
