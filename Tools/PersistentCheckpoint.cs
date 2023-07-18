66642f02994dce2889b83f93e93a80a5a2613cac
SkyMound <crafteurkiller@gmail.com>
2023-07-18 23:26:32 +0200

    feat: add checkpoint tool file (WIP)

---
+++ Tools/PersistentCheckpoint.cs
@@ -0,0 +1,37 @@
+﻿using DS.Game.Luna;
+using DS.Tech.Util;
+using DS.Game.Updraft;
+using System.Collections;
+using UnityEngine;
+
+namespace Tools
+{
+    public class PersistentCheckpoint : MonoBehaviour
+    {
+        public UpdraftRoomDoor persistentCheckpoint;
+
+
+        void SetCurrentToPersistent()
+        {
+            persistentCheckpoint.DoorNode = ServiceLocator.Instance.GetService<ServerLevelFlowScope>().SpawningDoorNode;
+        }
+
+        void LoadPersistent()
+        {
+            ServiceLocator.Instance.GetService<ServerLevelFlowScope>().OnCheckpointReached(persistentCheckpoint, true);
+
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            persistentCheckpoint = new UpdraftRoomDoor();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
