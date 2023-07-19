e3f1b5785dd6182abb7e492d7b581ea32baf326a
SkyMound <crafteurkiller@gmail.com>
2023-07-19 09:49:22 +0200

    feat: added main functions declerations

--- Tools/PersistentCheckpoint.cs
+++ Tools/PersistentCheckpoint.cs
@@ -1,6 +1,7 @@
 ﻿using DS.Game.Luna;
 using DS.Tech.Util;
 using DS.Game.Updraft;
+using System.IO;
 using System.Collections;
 using UnityEngine;
 
@@ -9,22 +10,43 @@ namespace Tools
     public class PersistentCheckpoint : MonoBehaviour
     {
         public UpdraftRoomDoor persistentCheckpoint;
+        public string pathToFolder;
+        public bool autoloadEnabled;
 
+        bool IsPersistentSet(){
+            return persistentCheckpoint.DoorNode != null;
+        }
 
         void SetCurrentToPersistent()
         {
-            persistentCheckpoint.DoorNode = ServiceLocator.Instance.GetService<ServerLevelFlowScope>().SpawningDoorNode;
+            persistentCheckpoint.DoorNode = ServiceLocator.Instance.GetService<ServerLevelFlowScope>().SpawningDoorNode; // It only save DoorNode, won't really work.
         }
 
         void LoadPersistent()
         {
-            ServiceLocator.Instance.GetService<ServerLevelFlowScope>().OnCheckpointReached(persistentCheckpoint, true);
+            if(IsPersistentSet()){
+                ServiceLocator.Instance.GetService<ServerLevelFlowScope>().OnCheckpointReached(persistentCheckpoint, true);
+            }
 
+        }
+
+        void SetSaveToPersistent(string name){
+
+        }
+
+        void SavePersistent(string name){
+
+        }
+
+        string[] GetSaves(){
+            return Directory.GetFiles(pathToFolder);
         }
 
         // Use this for initialization
         void Start()
         {
+            autoloadEnabled = false;
+            pathToFolder = Directory.GetCurrentDirectory();
             persistentCheckpoint = new UpdraftRoomDoor();
         }
 
