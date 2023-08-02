a9ed823c6a234d47f6417400d7b7ee7415573dbf
SkyMound <crafteurkiller@gmail.com>
2023-08-02 02:39:45 +0200

    feat: Label with good size and font

--- Tools/AbilityLogger.cs
+++ Tools/AbilityLogger.cs
@@ -1,4 +1,5 @@
 ﻿using DS.Game.Luna;
+using DS.Tech.App;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,11 +9,24 @@ namespace Tools
 {
     public class AbilityLogger : MonoBehaviour
     {
-        // Use this for initialization
+        GUIStyle _textStyle;
+
+        void OnGUI()
+        {
+            GUI.Label(new Rect((float)Screen.width-150, (float)Screen.height-25 , 150, 25), "ABCDE", this._textStyle);
+
+        }
+
 
         void Start()
         {
             Debugger.Log("Ability Logger");
+    
+            Font font = AssetBundleMap.Instance.LoadAsset<Font>("Assets/DS.Game.Updraft/UI/Fonts_RestOfWorld/FontAssets/FSLola-Medium.otf");
+            this._textStyle = new GUIStyle();
+            this._textStyle.font = font;
+            this._textStyle.fontSize = 30;
+            this._textStyle.normal.textColor = Color.white;
         }
 
         CommandFrame GetCommandFrame()
