ada30791c09ce0772a438cb8db6266095ab93113
SkyMound <crafteurkiller@gmail.com>
2023-08-19 01:09:23 +0200

    chore: initial file

---
+++ Tools/RouteManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tools
+{
+    public class RouteManager : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+    public class Route
+    {
+        List<Segment> segments;
+    }
+
+    public class Segment
+    {
+        List<Vector2> points;
+    }
+}
